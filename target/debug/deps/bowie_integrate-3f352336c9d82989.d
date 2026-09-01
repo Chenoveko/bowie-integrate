8b3774a79dc0d09246e663014d2674a64ba15184
cheno <jastorgaybarra@gmail.com>
2026-09-01 17:59:21 +0200

    Prepare crate

---
+++ target/debug/deps/bowie_integrate-3f352336c9d82989.d
@@ -0,0 +1,7 @@
+/home/jorge/Documents/bowie_project/bowie-integrate/target/debug/deps/bowie_integrate-3f352336c9d82989.d: src/lib.rs
+
+/home/jorge/Documents/bowie_project/bowie-integrate/target/debug/deps/libbowie_integrate-3f352336c9d82989.rlib: src/lib.rs
+
+/home/jorge/Documents/bowie_project/bowie-integrate/target/debug/deps/libbowie_integrate-3f352336c9d82989.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/libbowie_integrate.d
@@ -0,0 +1,1 @@
+/home/jorge/Documents/bowie_project/bowie-integrate/target/debug/libbowie_integrate.rlib: /home/jorge/Documents/bowie_project/bowie-integrate/target/package/bowie-integrate-0.1.0/src/lib.rs
