e9b6fac189591b5dc4f0b28de9a08a533bd23cc5
cheno <jastorgaybarra@gmail.com>
2026-09-01 17:57:58 +0200

    Prepare crate

---
+++ target/debug/deps/bowie_integrate-30f848f85ca3b1a3.d
@@ -0,0 +1,5 @@
+/home/jorge/Documents/bowie_project/bowie-integrate/target/debug/deps/bowie_integrate-30f848f85ca3b1a3.d: src/lib.rs
+
+/home/jorge/Documents/bowie_project/bowie-integrate/target/debug/deps/libbowie_integrate-30f848f85ca3b1a3.rmeta: src/lib.rs
+
+src/lib.rs:
